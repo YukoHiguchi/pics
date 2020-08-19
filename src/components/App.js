c7e7f219a78d3c41fa08c578a50428f500207d45
Yuko Higuchi <yhiguchi@storagevaultcanada.com>
2020-08-19 09:41:32 -0400

    API Requests with Reac

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar.js';
 
 class App extends React.Component {
-	onSearchSubmit(term) {
+	state = {images: []};
+	onSearchSubmit = async (term) => {
 		console.log(term);
-		axios
-			.get('https://api.unsplash.com/search/photos', {
-				params: {query: term},
-				headers: {
-					Authorization:
-						'Client-ID sEFBEifh1jraAb6ZF6yKLPiJmhHUUdQxVxaz0FMD0Ug',
-				},
-			})
-			.then((response) => {
-				console.log(response.data.result);
-			});
-	}
+		const response = await unsplash.get('/search/photos', {
+			params: {query: term},
+		});
+		this.setState({images: response.data.results});
+	};
 	render() {
 		return (
 			<div className="ui container" style={{marginTop: '10px'}}>
 				<SearchBar onSubmit={this.onSearchSubmit} />
+				Found: {this.state.images.length} images
 			</div>
 		);
 	}
